628aaf7923402d50df7bb35198804e4a762f005f
varma02 <varma01@mail.com>
2023-07-24 09:23:56 +0000

    add build script 🏗

---
+++ build.sh
@@ -0,0 +1,12 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d ' ')
+
+docker build -t ghcr.io/varma02/generic-music-bot3:latest .
+docker push ghcr.io/varma02/generic-music-bot3:latest
+
+docker build -t "ghcr.io/varma02/generic-music-bot3:$PACKAGE_VERSION" .
+docker push "ghcr.io/varma02/generic-music-bot3:$PACKAGE_VERSION"
